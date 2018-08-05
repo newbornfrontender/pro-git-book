69da5e87b1b7e7c7cf3e97496d3220d0737fb244
Infant Frontender <newbornfrontender@gmail.com>
2018-08-05 17:12:26 +0300

    Перенес pow в index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 'use strict';
 
-// +-------+-------------------------------------------------------------------+
-// | Title |                                                                   |
-// +-------+-------------------------------------------------------------------+
+const x: number = 5;
+const n: number = 3;
 
-import { showPowResult as pow } from './assets/pow';
+const getPowResult = (x: number, n: number): number => {
+  let result: number = x;
 
-pow();
+  for (let i = 1; i < n; i++) {
+    result = result * i;
+  };
 
-// -----------------------------------------------------------------------------
+  return result;
+};
+
+const showPowResult = () => {
+  if (n < 1) {
+    return console.log(`Степень ${n} не поддерживается, введите целую степень, большую 1`);
+  };
+
+  return console.log( getPowResult(x, n) );
+};
+
+showPowResult();
